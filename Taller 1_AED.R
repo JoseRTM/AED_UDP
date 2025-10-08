44082361e5fbadc10c04286a72585d99454e6db9
joseRTM <jose.ruiztaglem@gmail.com>
2025-10-08 18:16:13 -0300

    Taller 1-Descriptivo

---
+++ Taller 1_AED.R
@@ -0,0 +1,48 @@
+# GUÍA DE EJERCICIOS ESTADÍSTICA DESCRIPTIVA Y VISUALIZACIÓN
+# Active las librerías que va a usar
+
+# 1. Importe la base de datos jovenes.rds
+# Es la base de datos de la Encuesta Nacional de la Juventud
+# Encuesta poblacional realizada por INJUV
+# La población representada son jóvenes de 15 a 29 años.
+
+
+# 2. Guarde como objeto una base de datos reducida
+# Debe contener las variables: P77_1, P66 y P89
+# Hint: utilice []
+# Hint: La p89 es una variable que mide si ha vivido un embarazo no intencionado
+
+# 3. De un vistazo general de la base de datos
+# Hint: Puede usar str() o skim()
+
+# 4. Limpie las variables de ser necesario
+
+# 5. Etiquete las variables siguiendo la siguiente estructura:
+# p77_1:Frecuencia de consumo de alcohol
+# 1. Varias veces al día 
+# 2. Varias veces a la semana      
+# 3. Varias veces al mes   
+# 4. Una vez al mes o menos 
+# P66: Se identifica con alguna religión
+# 1. Si, 2. No 
+# p89: Embarazo no intencionado
+# 1. Si, 2. No
+
+# 6. Realice un análisis descriptivo completo de las variables. Construya un relato
+# breve comentando los resultados.
+# Hint: Utilice describe() para cuantitativas
+# Hint: Utilice table, tab1 o tabyl para las cualitativas
+
+# 7. Compare a través de un gráfico la proporción de embarazos no intencionados por
+# identificación religiosa.
+
+# 8. Compare a través de un gráfico la proporción de embarazos no intencionados por
+# frecuencia de consumo de alcohol.
+
+# 10. Compare a través un gráfico la frecuencia de consumo de alcohol por identificación religiosa
+# Discuta los resultados
+
+# 11. Compare a través un gráfico la proporción de consumo de alcohol por identificación religiosa
+# Discuta los resultados
+# Hint: Utilice margins = 1 para obtener las proporciones por fila
+# Discuta la diferencia entre este gráfico y el anterior.
